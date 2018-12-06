55ccadf59245e088a38ac0b8cd25e7e0eca66ae7
Chris Athanas <realityexpanderdev@gmail.com>
2018-12-06 09:27:26 -0600

    Create romanNumerals.js

---
+++ romanNumerals.js
@@ -0,0 +1,22 @@
+function translateRomanNumeral (romanNumeral) {
+  // Write your code here, and
+  // return your final answer.
+}
+
+var DIGIT_VALUES = {
+  I: 1,
+  V: 5,
+  X: 10,
+  L: 50,
+  C: 100,
+  D: 500,
+  M: 1000
+};
+
+console.log(translateRomanNumeral("X"))
+console.log(translateRomanNumeral("M"))
+console.log(translateRomanNumeral("VII"))
+console.log(translateRomanNumeral("D"))
+console.log(translateRomanNumeral("MCMX"))
+console.log(translateRomanNumeral("horse"))
+console.log(translateRomanNumeral("MDCCCCX"))
